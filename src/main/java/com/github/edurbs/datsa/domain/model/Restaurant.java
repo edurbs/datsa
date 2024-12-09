4b54328ad303fb05a034c00de7bd98a30049194c
Eduardo Soares <edurbs@gmail.com>
2024-12-09 20:50:05 -0300

    add sql import

--- src/main/java/com/github/edurbs/datsa/domain/model/Restaurant.java
+++ src/main/java/com/github/edurbs/datsa/domain/model/Restaurant.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -26,5 +27,8 @@ public class Restaurant {
 
     @Column
     private BigDecimal shippingFee;
+
+    @ManyToOne
+    private Kitchen kitchen;
 
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+insert into kitchen (id, name) values (1, 'Tailandesa');
+insert into kitchen (id, name) values (2, 'Indiana');
+
+insert into restaurant (name, shipping_fee, kitchen_id) values ('Thai Gourmet', 10, 1);
+insert into restaurant (name, shipping_fee, kitchen_id) values ('Thai Delivery', 9.50, 1);
+insert into restaurant (name, shipping_fee, kitchen_id) values ('Tuk Tuk Comida Indiana', 15, 2);
