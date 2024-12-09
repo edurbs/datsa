36a336d0e24e0182b06c650d165c31387f33035a
Eduardo Soares <edurbs@gmail.com>
2024-12-08 23:08:34 -0300

    create spring boot project

---
+++ src/main/java/com/github/edurbs/datsa/DatsaApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.edurbs.datsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatsaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatsaApiApplication.class, args);
+	}
+
+}
