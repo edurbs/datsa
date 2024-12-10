e2e429651608e8aed03c0cea6558ffa6de5c403d
Eduardo Soares <edurbs@gmail.com>
2024-12-09 23:47:12 -0300

    add sonarqube

--- src/main/java/com/github/edurbs/datsa/DatsaApiApplication.java
+++ src/main/java/com/github/edurbs/datsa/DatsaApiApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DatsaApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DatsaApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DatsaApiApplication.class, args);
+
+    }
 
 }
