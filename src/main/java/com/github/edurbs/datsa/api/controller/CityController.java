da07a74670a89528db2b2bc0895bbefd7ff041f0
Eduardo Soares <edurbs@gmail.com>
2024-12-15 10:46:44 -0300

    add city controller

---
+++ src/main/java/com/github/edurbs/datsa/api/controller/CityController.java
@@ -0,0 +1,57 @@
+package com.github.edurbs.datsa.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.github.edurbs.datsa.domain.model.City;
+import com.github.edurbs.datsa.domain.service.CityRegistryService;
+
+@RestController
+@RequestMapping("/cities")
+public class CityController {
+    private static final String CITY_URL = "/cities";
+
+    @Autowired
+    private CityRegistryService cityRegistryService;
+
+    @GetMapping
+    public List<City> listAll() {
+        return cityRegistryService.getAll();
+    }
+
+    @GetMapping("/{cityId}")
+    public City getById(@PathVariable Long cityId) {
+        return cityRegistryService.getById(cityId);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public City add(@RequestBody City city) {
+        return cityRegistryService.save(city);
+    }
+
+    @PutMapping("/{cityId}")
+    public City alter(@PathVariable Long cityId, @RequestBody City city) {
+        var alteredCity = cityRegistryService.getById(cityId);
+        BeanUtils.copyProperties(city, alteredCity, "id");
+        return cityRegistryService.save(alteredCity);
+    }
+
+    @DeleteMapping("/{cityId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long cityId) {
+        cityRegistryService.remove(cityId);
+    }
+}
---
+++ src/test/java/com/github/edurbs/datsa/api/controller/CityControllerTest.java
@@ -0,0 +1,134 @@
+package com.github.edurbs.datsa.api.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.instancio.Instancio;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.github.edurbs.datsa.domain.exception.ModelInUseException;
+import com.github.edurbs.datsa.domain.exception.ModelNotFoundException;
+import com.github.edurbs.datsa.domain.exception.ModelValidationException;
+import com.github.edurbs.datsa.domain.model.City;
+import com.github.edurbs.datsa.domain.service.CityRegistryService;
+
+@WebMvcTest(CityController.class)
+class CityControllerTest {
+
+    private static final String CITY_URL = "/cities";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CityRegistryService cityRegistryService;
+
+    @Test
+    void whenGetInvalidUrl_thenStatus404() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/invalid"))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    void whenGetAll_thenStatus200() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get(CITY_URL))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    void whenGetValidCityId_thenStatus200() throws Exception {
+        Mockito.when(cityRegistryService.getById(1L)).thenReturn(Instancio.create(City.class));
+
+        mockMvc.perform(MockMvcRequestBuilders.get(CITY_URL + "/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    void whenGetInvalidCityId_thenStatus404() throws Exception {
+        Mockito.when(cityRegistryService.getById(999L)).thenThrow(new ModelNotFoundException());
+
+        mockMvc.perform(MockMvcRequestBuilders.get(CITY_URL + "/999"))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    void whenAddValidCity_thenStatus201() throws Exception {
+        var cityTest = Instancio.create(City.class);
+        Mockito.when(cityRegistryService.save(Mockito.any(City.class))).thenReturn(cityTest);
+
+        mockMvc.perform(MockMvcRequestBuilders.post(CITY_URL)
+                .content("""
+                        {
+                            "name":"%s",
+                            "state": {
+                                "id": 1
+                            }
+                        }
+                        """.formatted(cityTest.getName()))
+                .contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+    }
+
+    @Test
+    void whenAlterValidCity_thenStatus200() throws Exception {
+        var cityOriginal = new City();
+        cityOriginal.setId(1L);
+        cityOriginal.setName("some name");
+
+        var cityUpdated = new City();
+        cityUpdated.setName("new name");
+
+        Mockito.when(cityRegistryService.getById(cityOriginal.getId())).thenReturn(cityOriginal);
+        Mockito.when(cityRegistryService.save(Mockito.any(City.class))).thenReturn(cityOriginal);
+
+        mockMvc.perform(MockMvcRequestBuilders.put(CITY_URL + "/" + cityOriginal.getId())
+                .content("""
+                        {
+                            "name":"%s"
+                        }
+                        """.formatted(cityUpdated.getName())).contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+        ArgumentCaptor<City> cityCaptor = ArgumentCaptor.forClass(City.class);
+        Mockito.verify(cityRegistryService).save(cityCaptor.capture());
+        var alteredCity = cityCaptor.getValue();
+        assertEquals(cityUpdated.getName(), alteredCity.getName());
+        assertEquals(cityOriginal.getId(), alteredCity.getId());
+    }
+
+    @Test
+    void whenDeleteValidCity_thenStatus204() throws Exception {
+        Mockito.doNothing().when(cityRegistryService).remove(1L);
+        mockMvc.perform(MockMvcRequestBuilders.delete(CITY_URL + "/1"))
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+
+    @Test
+    void whenDeleteInvalidCity_thenStatus404() throws Exception {
+        Mockito.doThrow(new ModelNotFoundException()).when(cityRegistryService).remove(1L);
+        mockMvc.perform(MockMvcRequestBuilders.delete(CITY_URL + "/1"))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    void whenDeleteModelInUse_thenStatus409() throws Exception {
+        Mockito.doThrow(new ModelInUseException()).when(cityRegistryService).remove(1L);
+        mockMvc.perform(MockMvcRequestBuilders.delete(CITY_URL + "/1"))
+                .andExpect(MockMvcResultMatchers.status().isConflict());
+    }
+
+    @Test
+    void whenDeleteCityWithInvalidState_thenStatus400() throws Exception {
+        Mockito.doThrow(new ModelValidationException()).when(cityRegistryService).remove(1L);
+        mockMvc.perform(MockMvcRequestBuilders.delete(CITY_URL + "/1"))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+}
+
