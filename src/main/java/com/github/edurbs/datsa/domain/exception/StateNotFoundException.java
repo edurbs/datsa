b7a2efcf041c1f0e23a724d7fbcbc02d5bd19e89
Eduardo Soares <edurbs@gmail.com>
2024-12-31 20:59:00 -0300

    add default msg

--- src/main/java/com/github/edurbs/datsa/domain/exception/StateNotFoundException.java
+++ src/main/java/com/github/edurbs/datsa/domain/exception/StateNotFoundException.java
@@ -5,4 +5,8 @@ public class StateNotFoundException extends ModelNotFoundException {
     public StateNotFoundException(String msg){
         super(msg);
     }
+
+    public StateNotFoundException(Long id){
+        super("State id %d does not exist".formatted(id));
+    }
 }
