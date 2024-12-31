a30a3cf73522764fbf68f12cb7d361e5585a026b
Eduardo Soares <edurbs@gmail.com>
2024-12-31 20:59:19 -0300

    add user registry

--- src/main/java/com/github/edurbs/datsa/domain/service/UserRegistryService.java
+++ src/main/java/com/github/edurbs/datsa/domain/service/UserRegistryService.java
@@ -1,5 +1,51 @@
 package com.github.edurbs.datsa.domain.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.github.edurbs.datsa.domain.exception.ModelInUseException;
+import com.github.edurbs.datsa.domain.exception.UserNotFoundException;
+import com.github.edurbs.datsa.domain.model.User;
+import com.github.edurbs.datsa.domain.repository.UserRepository;
+
+@Service
 public class UserRegistryService {
 
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> getAll(){
+        return repository.findAll();
+    }
+
+    public User getById(Long id){
+        return repository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @Transactional
+    public void remove(Long id){
+        if(notExists(id)){
+            throw new UserNotFoundException(id);
+        }
+        try {
+            repository.deleteById(id);
+            repository.flush();;
+        } catch ( DataIntegrityViolationException e) {
+            throw new ModelInUseException("User id %d is in use and can not be removed.".formatted(id));
+        }
+    }
+
+    @Transactional
+    public User save(User user){
+        return repository.save(user);
+    }
+
+    private boolean notExists(Long id){
+        return !repository.existsById(id);
+    }
 }
