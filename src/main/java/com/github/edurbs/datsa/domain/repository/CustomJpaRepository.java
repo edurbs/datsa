a30fb5c55a409303c609bd121184fc8aaa1ba1ce
Eduardo Soares <edurbs@gmail.com>
2025-01-01 19:29:53 -0300

    fix decorator

--- src/main/java/com/github/edurbs/datsa/domain/repository/CustomJpaRepository.java
+++ src/main/java/com/github/edurbs/datsa/domain/repository/CustomJpaRepository.java
@@ -1,7 +1,9 @@
 package com.github.edurbs.datsa.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
+@NoRepositoryBean
 public interface CustomJpaRepository<T, I> extends JpaRepository<T, I> {
 
     void detach(T entity);
