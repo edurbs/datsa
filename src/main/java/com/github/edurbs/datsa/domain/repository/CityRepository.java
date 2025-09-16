6d0cdc753be9c0ae686947c90afb1688a05ae998
Eduardo Soares <edurbs@gmail.com>
2025-09-16 13:38:33 -0300

    fix problem n+1

--- src/main/java/com/github/edurbs/datsa/domain/repository/CityRepository.java
+++ src/main/java/com/github/edurbs/datsa/domain/repository/CityRepository.java
@@ -1,6 +1,10 @@
 package com.github.edurbs.datsa.domain.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
 import com.github.edurbs.datsa.domain.model.City;
@@ -8,6 +12,10 @@ import com.github.edurbs.datsa.domain.model.City;
 @Repository
 public interface CityRepository extends JpaRepository<City, Long> {
 
+    @EntityGraph(attributePaths = {"state"}) // fix problem n+1
+    @NonNull List<City> findAll();
+
     Long countByStateId(Long stateId);
+
 
 }
