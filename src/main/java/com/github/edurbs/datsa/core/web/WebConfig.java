520f80176272d9f6ff87c07c96e1c56249fae64a
Eduardo Soares <edurbs@gmail.com>
2025-09-21 20:27:14 -0300

    add api dead handler

--- src/main/java/com/github/edurbs/datsa/core/web/WebConfig.java
+++ src/main/java/com/github/edurbs/datsa/core/web/WebConfig.java
@@ -2,16 +2,20 @@ package com.github.edurbs.datsa.core.web;
 
 import javax.servlet.Filter;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private ApiDeadHandler apiDeprecationHandler;
 
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
@@ -27,8 +31,7 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
     @Override
-    public void configureContentNegotiation(ContentNegotiationConfigurer configurer){
-        configurer.defaultContentType(MyMediaTypes.V2_APPLICATION_JSON);
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(apiDeprecationHandler);
     }
-
 }
