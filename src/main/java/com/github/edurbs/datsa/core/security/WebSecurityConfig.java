374de4af082f5d7cf79ae646433e5786ef172bf4
Eduardo Soares <edurbs@gmail.com>
2025-09-22 14:40:14 -0300

    add basic auth

--- src/main/java/com/github/edurbs/datsa/core/security/WebSecurityConfig.java
+++ src/main/java/com/github/edurbs/datsa/core/security/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
@@ -11,10 +12,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic()
+        http.httpBasic() // basic authentication
             .and()
             .authorizeRequests()
-                //.antMatchers("/vi/kitchens/**").permitAll()
-                .anyRequest().authenticated();
+                .antMatchers("/v1/kitchens/**").permitAll() // this url accepts without authentication
+                .anyRequest().authenticated() // must be basic authenticated
+            .and()
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // not use cookies to identify the user
+            .and()
+                .csrf().disable(); // to accept put
     }
 }
