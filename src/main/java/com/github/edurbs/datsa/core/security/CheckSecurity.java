acea1e9fe04d7092767518be4ae052adf4648fd6
Eduardo Soares <edurbs@gmail.com>
2025-09-30 15:37:20 -0300

    add: restrict access with scopes

--- src/main/java/com/github/edurbs/datsa/core/security/CheckSecurity.java
+++ src/main/java/com/github/edurbs/datsa/core/security/CheckSecurity.java
@@ -10,12 +10,12 @@ import org.springframework.security.access.prepost.PreAuthorize;
 public @interface CheckSecurity {
 
     public @interface Kitchens{
-        @PreAuthorize("hasAuthority('EDIT_KITCHENS')")
+        @PreAuthorize("hasAuthority('SCOPE_WRITE') and hasAuthority('EDIT_KITCHENS')")
         @Retention(RetentionPolicy.RUNTIME)
         @Target(ElementType.METHOD)
         public @interface CanEdit {}
 
-        @PreAuthorize("hasAuthority('CONSULT_KITCHENS')")
+        @PreAuthorize("hasAuthority('SCOPE_READ') and hasAuthority('CONSULT_KITCHENS')")
         @Retention(RetentionPolicy.RUNTIME)
         @Target(ElementType.METHOD)
         public @interface CanConsult {}
