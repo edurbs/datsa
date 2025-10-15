1e0173a8dcc9055484e07b48775b2d0051121524
Eduardo Soares <edurbs@gmail.com>
2025-10-14 21:19:32 -0300

    add: add problem object to springdoc

--- src/main/java/com/github/edurbs/datsa/core/springdoc/SpringDocConfig.java
+++ src/main/java/com/github/edurbs/datsa/core/springdoc/SpringDocConfig.java
@@ -1,7 +1,6 @@
 package com.github.edurbs.datsa.core.springdoc;
 
 import com.github.edurbs.datsa.api.exceptionhandler.Problem;
-import com.github.edurbs.datsa.api.exceptionhandler.ProblemType;
 import io.swagger.v3.core.converter.ModelConverters;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.OAuthFlow;
@@ -13,6 +12,8 @@ import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.MediaType;
 import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.responses.ApiResponse;
 import io.swagger.v3.oas.models.responses.ApiResponses;
@@ -22,6 +23,8 @@ import org.springframework.context.annotation.Configuration;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @Configuration
 @SecurityScheme(name = "security_auth",
@@ -33,13 +36,13 @@ import java.util.Map;
             @OAuthScope(name = "READ", description = "read scope"),
             @OAuthScope(name = "WRITE", description = "write scope"),
         }
-)))
+    )))
 public class SpringDocConfig {
 
-    private HashMap<String, ApiResponse> responseMap ;
+    private HashMap<String, ApiResponse> responseMap;
 
     @Bean
-    public OpenAPI openAPI(){
+    public OpenAPI openAPI() {
         return new OpenAPI()
             .info(new Info()
                 .title("Datsa API")
@@ -52,23 +55,23 @@ public class SpringDocConfig {
             ).externalDocs(new ExternalDocumentation()
                 .description("Datsa docs")
                 .url("http://docs.datsa.com.br")
-            ).components(new Components().schemas(
-                getSchemas()
-            ))
-            ;
+            ).components(new Components()
+                .schemas(getSchemas())
+                .responses(getResponseMap())
+            );
     }
 
-    private Map<String, Schema> getSchemas(){
+    private Map<String, Schema> getSchemas() {
         final Map<String, Schema> schemaMap = new HashMap<>();
         Map<String, Schema> problemSchema = ModelConverters.getInstance().read(Problem.class);
-        Map<String, Schema> problemTypeSchema = ModelConverters.getInstance().read(ProblemType.class);
+        Map<String, Schema> problemFieldSchema = ModelConverters.getInstance().read(Problem.Field.class);
         schemaMap.putAll(problemSchema);
-        schemaMap.putAll(problemTypeSchema);
+        schemaMap.putAll(problemFieldSchema);
         return schemaMap;
     }
 
     @Bean
-    public OpenApiCustomiser openApiCustomiser(){
+    public OpenApiCustomiser openApiCustomiser() {
         return openApi -> {
             openApi.getPaths()
                 .values()
@@ -104,22 +107,35 @@ public class SpringDocConfig {
         };
     }
 
-    private HashMap<String, ApiResponse> getResponse(String code){
+    private HashMap<String, ApiResponse> getResponse(String code) {
         HashMap<String, ApiResponse> responseMap = new HashMap<>();
         responseMap.put(code, getResponseMap().get(code));
         return responseMap;
     }
 
-    private HashMap<String, ApiResponse> getResponseMap(){
-        if(responseMap != null){
+    private HashMap<String, ApiResponse> getResponseMap() {
+        if (responseMap != null) {
             return responseMap;
         }
         responseMap = new HashMap<>();
-        responseMap.put("400", new ApiResponse().description("Invalid request"));
-        responseMap.put("404", new ApiResponse().description("Resource not found"));
-        responseMap.put("406", new ApiResponse().description("No acceptable representation found for the requested resource"));
-        responseMap.put("500", new ApiResponse().description("Server internal error"));
-        return  responseMap;
+        responseMap.put("400", createResponse("Invalid request"));
+        responseMap.put("404", createResponse("Resource not found"));
+        responseMap.put("406", createResponse("No acceptable representation found for the requested resource"));
+        responseMap.put("500", createResponse("Server internal error"));
+        return responseMap;
     }
+
+    private ApiResponse createResponse(String description) {
+        Schema<?> problemSchema = new Schema<Problem>()
+            .$ref("Problem");
+        MediaType mediaType = new MediaType()
+            .schema(problemSchema);
+        Content problemContent = new Content()
+            .addMediaType(APPLICATION_JSON_VALUE, mediaType);
+        return new ApiResponse()
+            .description(description)
+            .content(problemContent);
+    }
+
 
 }
