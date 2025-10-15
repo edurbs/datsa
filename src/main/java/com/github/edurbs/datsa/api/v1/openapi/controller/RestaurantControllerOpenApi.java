71a2d22ceec36646ff35f4623c7acf52e4632aab
Eduardo Soares <edurbs@gmail.com>
2025-10-15 15:05:11 -0300

    add: restaurant list projection parameters to springdoc

--- src/main/java/com/github/edurbs/datsa/api/v1/openapi/controller/RestaurantControllerOpenApi.java
+++ src/main/java/com/github/edurbs/datsa/api/v1/openapi/controller/RestaurantControllerOpenApi.java
@@ -4,41 +4,52 @@ import com.github.edurbs.datsa.api.v1.dto.input.RestaurantInput;
 import com.github.edurbs.datsa.api.v1.dto.output.RestaurantNameOutput;
 import com.github.edurbs.datsa.api.v1.dto.output.RestaurantOutput;
 import com.github.edurbs.datsa.api.v1.dto.output.RestaurantSummaryOutput;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @SecurityRequirement(name="security_auth")
 @Tag(name = "Restaurants", description = "Restaurants registry")
 public interface RestaurantControllerOpenApi {
+
+    @Operation(parameters = {
+        @Parameter(
+            name = "projection",
+            description = "Projection name",
+            example = "only-name",
+            in = ParameterIn.QUERY,
+            required = false
+        )
+    })
     CollectionModel<RestaurantSummaryOutput> listAll();
 
+    @Operation(hidden = true)
     CollectionModel<RestaurantNameOutput> listAllOnlyName();
 
-    RestaurantOutput getById(@PathVariable Long restaurantId);
+    RestaurantOutput getById(Long restaurantId);
 
-    ResponseEntity<RestaurantOutput> add(@RequestBody @Valid RestaurantInput restaurantInput);
+    ResponseEntity<RestaurantOutput> add(RestaurantInput restaurantInput);
 
-    RestaurantOutput alter(@PathVariable Long restaurantId,
-                           @RequestBody @Valid RestaurantInput restaurantInput);
+    RestaurantOutput alter(Long restaurantId,
+                           RestaurantInput restaurantInput);
 
-    void delete(@PathVariable Long restaurantId);
+    void delete(Long restaurantId);
 
-    ResponseEntity<Void> activate(@PathVariable Long restaurantId);
+    ResponseEntity<Void> activate(Long restaurantId);
 
-    ResponseEntity<Void> activations(@RequestBody List<Long> restaurantIds);
+    ResponseEntity<Void> activations(List<Long> restaurantIds);
 
-    ResponseEntity<Void> inactivate(@PathVariable Long restaurantId);
+    ResponseEntity<Void> inactivate(Long restaurantId);
 
-    ResponseEntity<Void> inactivations(@RequestBody List<Long> restaurantIds);
+    ResponseEntity<Void> inactivations(List<Long> restaurantIds);
 
-    ResponseEntity<Void> open(@PathVariable Long restaurantId);
+    ResponseEntity<Void> open(Long restaurantId);
 
-    ResponseEntity<Void> close(@PathVariable Long restaurantId);
+    ResponseEntity<Void> close(Long restaurantId);
 }
