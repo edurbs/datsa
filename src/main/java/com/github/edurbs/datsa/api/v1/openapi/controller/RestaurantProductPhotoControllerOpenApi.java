ccba3d82ba26f92489788ad3c0e8203b062b0144
Eduardo Soares <edurbs@gmail.com>
2025-10-15 15:12:41 -0300

    add: content type png, jpg and json to product photo documentation in springdoc

--- src/main/java/com/github/edurbs/datsa/api/v1/openapi/controller/RestaurantProductPhotoControllerOpenApi.java
+++ src/main/java/com/github/edurbs/datsa/api/v1/openapi/controller/RestaurantProductPhotoControllerOpenApi.java
@@ -2,27 +2,36 @@ package com.github.edurbs.datsa.api.v1.openapi.controller;
 
 import com.github.edurbs.datsa.api.v1.dto.input.ProductPhotoInput;
 import com.github.edurbs.datsa.api.v1.dto.output.ProductPhotoOutput;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import javax.validation.Valid;
 import java.io.IOException;
 
 @SecurityRequirement(name="security_auth")
 @Tag(name = "Restaurants")
 public interface RestaurantProductPhotoControllerOpenApi {
-    ProductPhotoOutput updatePhoto(@PathVariable Long restaurantId, @PathVariable Long productId, @Valid ProductPhotoInput productPhotoInput) throws IOException;
+    ProductPhotoOutput updatePhoto(Long restaurantId, Long productId, ProductPhotoInput productPhotoInput) throws IOException;
 
-    ProductPhotoOutput getPhoto(@PathVariable Long restaurantId, @PathVariable Long productId);
+    ResponseEntity<ResponseStatus> delete(Long restaurantId, Long productId);
 
-    ResponseEntity<ResponseStatus> delete(@PathVariable Long restaurantId, @PathVariable Long productId);
+    @Operation(summary = "Get a product photo from a restaurant", responses = {
+        @ApiResponse(responseCode = "200", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = ProductPhotoOutput.class)),
+            @Content(mediaType = "image/jpeg", schema = @Schema(type = "string", format = "binary")),
+            @Content(mediaType = "image/png", schema = @Schema(type = "string", format = "binary"))
+        })
+    })
+    ProductPhotoOutput getPhoto(Long restaurantId, Long productId);
 
-    ResponseEntity<?> getPhotoData(@PathVariable Long restaurantId, @PathVariable Long productId, @RequestHeader(name = "accept") String acceptHeader) throws HttpMediaTypeNotAcceptableException;
+    @Operation(hidden = true)
+    ResponseEntity<?> getPhotoData(Long restaurantId, Long productId, String acceptHeader) throws HttpMediaTypeNotAcceptableException;
 
 
 }
