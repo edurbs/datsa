9a63305d3b79097e6c0c321289824c2d1773bf1a
Eduardo Soares <edurbs@gmail.com>
2025-10-15 19:10:39 -0300

    add: permissions documentation in springdoc

--- src/main/java/com/github/edurbs/datsa/api/v1/openapi/controller/PermissionControllerOpenApi.java
+++ src/main/java/com/github/edurbs/datsa/api/v1/openapi/controller/PermissionControllerOpenApi.java
@@ -1,6 +1,7 @@
 package com.github.edurbs.datsa.api.v1.openapi.controller;
 
 import com.github.edurbs.datsa.api.v1.dto.output.PermissionOutput;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.hateoas.CollectionModel;
@@ -8,5 +9,7 @@ import org.springframework.hateoas.CollectionModel;
 @SecurityRequirement(name="security_auth")
 @Tag(name = "Permissions", description = "Permissions registry")
 public interface PermissionControllerOpenApi {
+
+    @Operation(summary = "List permissions")
     CollectionModel<PermissionOutput> findAll();
 }
