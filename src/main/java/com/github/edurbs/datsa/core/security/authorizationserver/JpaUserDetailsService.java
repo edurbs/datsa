ff2a5fac756be3fd392fb91f3dc30653c2cc51b5
Eduardo Soares <edurbs@gmail.com>
2025-10-16 11:04:57 -0300

    add: authorization core in auth server

--- src/main/java/com/github/edurbs/datsa/core/security/authorizationserver/JpaUserDetailsService.java
+++ src/main/java/com/github/edurbs/datsa/core/security/authorizationserver/JpaUserDetailsService.java
@@ -1,19 +1,19 @@
 package com.github.edurbs.datsa.core.security.authorizationserver;
 
-import java.util.Collection;
-import java.util.stream.Collectors;
-
+import com.github.edurbs.datsa.domain.model.MyUser;
+import com.github.edurbs.datsa.domain.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.github.edurbs.datsa.domain.model.MyUser;
-import com.github.edurbs.datsa.domain.repository.UserRepository;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 @Service
 public class JpaUserDetailsService implements UserDetailsService {
@@ -26,7 +26,7 @@ public class JpaUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         MyUser myUser = userRepository.findByEmail(username)
             .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
-        return new AuthUser(myUser, getAuthorities(myUser));
+        return new User(myUser.getEmail(), myUser.getPassword(), getAuthorities(myUser));
     }
 
     private Collection<GrantedAuthority> getAuthorities(MyUser myUser){
