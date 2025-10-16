ac26fbf4e29985e69678ba94e97dda65d9e012d8
Eduardo Soares <edurbs@gmail.com>
2025-10-15 23:45:12 -0300

    add: auth server with opaque token

---
+++ src/main/java/com/github/edurbs/datsa/core/security/authorizationserver/DatsaSecurityProperties.java
@@ -0,0 +1,20 @@
+package com.github.edurbs.datsa.core.security.authorizationserver;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotBlank;
+
+@Component
+@Getter
+@Setter
+@Validated
+@ConfigurationProperties("datsa.auth")
+public class DatsaSecurityProperties {
+
+    @NotBlank
+    private String providerUrl;
+}
