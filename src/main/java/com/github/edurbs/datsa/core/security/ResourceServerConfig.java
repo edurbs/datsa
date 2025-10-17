c626c9cb41ba79b07b3bf529a05fb8bf441c2e7e
Eduardo Soares <edurbs@gmail.com>
2025-10-17 00:57:37 -0300

    upgrade spring authorization server

--- src/main/java/com/github/edurbs/datsa/core/security/ResourceServerConfig.java
+++ src/main/java/com/github/edurbs/datsa/core/security/ResourceServerConfig.java
@@ -2,9 +2,10 @@ package com.github.edurbs.datsa.core.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
@@ -13,41 +14,39 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
 public class ResourceServerConfig {
 
     @Bean
     public SecurityFilterChain resourceServerFilterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeRequests()
-                .antMatchers("/oauth2/**").authenticated()
-            .and()
-                .csrf().disable().cors()
-            .and()
-                .oauth2ResourceServer()
-                .jwt()
-                .jwtAuthenticationConverter(jwtAuthenticationConverter());
+                .formLogin(customizer -> customizer.loginPage("/login")) // only needed with the Auth Server and Resource server are together
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors->{})
+                .oauth2ResourceServer(oauth2 -> oauth2
+                    .jwt(jwt -> jwt
+                        .jwtAuthenticationConverter(jwtAuthenticationConverter())));
 
-        return http
-                .formLogin(customizer -> customizer.loginPage("/login"))
-                .build();
+        return http.build();
     }
 
     private JwtAuthenticationConverter jwtAuthenticationConverter() {
         JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
 
         converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            //"authorities" is a custom field of AuthorizationServerConfig
+            List<String> authorities = jwt.getClaimAsStringList("authorities");
+
             JwtGrantedAuthoritiesConverter authoritiesConverter = new JwtGrantedAuthoritiesConverter();
             // read the permissions in the scopes
             Collection<GrantedAuthority> grantedAuthorities  = authoritiesConverter.convert(jwt);
 
-            //"authorities" is a custom field of AuthorizationServerConfig
-            List<String> authorities = jwt.getClaimAsStringList("authorities");
 
-            if (authorities == null) {
+            if (Objects.isNull(authorities)) {
                 return grantedAuthorities; // return the scopes list to the client credential authorization
             }
 
---
+++ src/main/java/com/github/edurbs/datsa/core/security/authorizationserver/LoginController.java
@@ -0,0 +1,13 @@
+package com.github.edurbs.datsa.core.security.authorizationserver;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(){
+        return "pages/login";
+    }
+}
