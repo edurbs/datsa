6aa2f17aa0410286973e7de9719911f641536d04
Eduardo Soares <edurbs@gmail.com>
2025-10-31 16:08:33 -0300

    add: override equals method

--- src/main/java/com/github/edurbs/datsa/api/v1/dto/output/UserOutput.java
+++ src/main/java/com/github/edurbs/datsa/api/v1/dto/output/UserOutput.java
@@ -1,15 +1,16 @@
 package com.github.edurbs.datsa.api.v1.dto.output;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import org.springframework.hateoas.RepresentationModel;
-import org.springframework.hateoas.server.core.Relation;
-
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
 
 @Relation(collectionRelation = "users")
 @Setter
 @Getter
+@EqualsAndHashCode(callSuper = false)
 public class UserOutput extends RepresentationModel<UserOutput> {
 
     @Schema(example = "1")
