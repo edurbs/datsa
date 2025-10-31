149c4c5e3ad5c2a626af6c404d9890588014da19
Eduardo Soares <edurbs@gmail.com>
2025-10-31 15:51:09 -0300

    add: constructor injection

--- src/main/java/com/github/edurbs/datsa/api/v2/controller/KitchenControllerV2.java
+++ src/main/java/com/github/edurbs/datsa/api/v2/controller/KitchenControllerV2.java
@@ -5,7 +5,7 @@ import com.github.edurbs.datsa.api.v2.dto.output.KitchenOutputV2;
 import com.github.edurbs.datsa.api.v2.mapper.KitchenMapperV2;
 import com.github.edurbs.datsa.domain.model.Kitchen;
 import com.github.edurbs.datsa.domain.service.KitchenRegistryService;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PagedResourcesAssembler;
@@ -13,20 +13,21 @@ import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import jakarta.validation.Valid;
-
 @RestController
 @RequestMapping("/v2/kitchens")
 public class KitchenControllerV2 {
 
-    @Autowired
-    private KitchenRegistryService kitchenRegistryService;
+    private final KitchenRegistryService kitchenRegistryService;
 
-    @Autowired
-    private KitchenMapperV2 kitchenMapperV2;
+    private final KitchenMapperV2 kitchenMapperV2;
 
-    @Autowired
-    private PagedResourcesAssembler<Kitchen> pagedResourcesAssembler;
+    private final PagedResourcesAssembler<Kitchen> pagedResourcesAssembler;
+
+    public KitchenControllerV2(KitchenRegistryService kitchenRegistryService, KitchenMapperV2 kitchenMapperV2, PagedResourcesAssembler<Kitchen> pagedResourcesAssembler) {
+        this.kitchenRegistryService = kitchenRegistryService;
+        this.kitchenMapperV2 = kitchenMapperV2;
+        this.pagedResourcesAssembler = pagedResourcesAssembler;
+    }
 
     @GetMapping()
     public PagedModel<KitchenOutputV2> listAll(Pageable pageable) {
