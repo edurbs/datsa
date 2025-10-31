16c8c33694f4b608239edb35868e431fcfa627a3
Eduardo Soares <edurbs@gmail.com>
2025-10-31 15:47:25 -0300

    add: equals implementation

--- src/main/java/com/github/edurbs/datsa/api/v1/dto/output/CityOutput.java
+++ src/main/java/com/github/edurbs/datsa/api/v1/dto/output/CityOutput.java
@@ -1,16 +1,17 @@
 package com.github.edurbs.datsa.api.v1.dto.output;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import org.springframework.hateoas.RepresentationModel;
-import org.springframework.hateoas.server.core.Relation;
-
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
 
 
 @Relation(collectionRelation = "cities")
 @Getter
 @Setter
+@EqualsAndHashCode(callSuper = false)
 public class CityOutput extends RepresentationModel<CityOutput> {
 
     @Schema(example = "1")
--- src/main/java/com/github/edurbs/datsa/api/v2/dto/output/CityOutputV2.java
+++ src/main/java/com/github/edurbs/datsa/api/v2/dto/output/CityOutputV2.java
@@ -1,5 +1,6 @@
 package com.github.edurbs.datsa.api.v2.dto.output;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.hateoas.RepresentationModel;
@@ -8,11 +9,11 @@ import org.springframework.hateoas.server.core.Relation;
 @Relation(collectionRelation = "cities")
 @Getter
 @Setter
+@EqualsAndHashCode(callSuper = true)
 public class CityOutputV2 extends RepresentationModel<CityOutputV2> {
 
     private Long cityId;
     private String cityName;
     private Long stateId;
     private String stateName;
-//    private StateOutput state;
 }
