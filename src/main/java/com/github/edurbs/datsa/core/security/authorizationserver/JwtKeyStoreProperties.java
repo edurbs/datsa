732b3ba58d9624c472b923ffcad7078fbd5f6340
Eduardo Soares <edurbs@gmail.com>
2025-10-31 16:26:18 -0300

    remove: unused imports

--- src/main/java/com/github/edurbs/datsa/core/security/authorizationserver/JwtKeyStoreProperties.java
+++ src/main/java/com/github/edurbs/datsa/core/security/authorizationserver/JwtKeyStoreProperties.java
@@ -1,17 +1,14 @@
 package com.github.edurbs.datsa.core.security.authorizationserver;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.core.io.Resource;
-import org.springframework.stereotype.Component;
-import org.springframework.validation.annotation.Validated;
-
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 @Validated
 @Component
