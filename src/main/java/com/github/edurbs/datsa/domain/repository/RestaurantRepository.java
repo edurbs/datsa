3f4c73c506d3c983dc460178ddca85a1c6e80ee9
Eduardo Soares <edurbs@gmail.com>
2025-10-31 16:19:41 -0300

    fix: comment

--- src/main/java/com/github/edurbs/datsa/domain/repository/RestaurantRepository.java
+++ src/main/java/com/github/edurbs/datsa/domain/repository/RestaurantRepository.java
@@ -1,23 +1,18 @@
 package com.github.edurbs.datsa.domain.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
-
 import com.github.edurbs.datsa.domain.model.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long>{
 
-    // @Query("""
+    // This query do this in plain sql:
     //         select case when count(1) > 0 then true else false end
     //         from Restaurant r
     //         join r.users u
     //         where r.id = :restaurantId
     //         and u.id = : userId
-    //         """)
-    // boolean userExists(Long restaurantId, Long userId);
-
     boolean existsByIdAndUsers_Id(Long restaurantId, Long userId);
 
 }
