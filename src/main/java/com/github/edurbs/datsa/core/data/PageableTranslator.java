416f62e68b49cbfa28852905a3b5840565033811
Eduardo Soares <edurbs@gmail.com>
2025-10-31 16:21:50 -0300

    add: private constructor

--- src/main/java/com/github/edurbs/datsa/core/data/PageableTranslator.java
+++ src/main/java/com/github/edurbs/datsa/core/data/PageableTranslator.java
@@ -1,12 +1,15 @@
 package com.github.edurbs.datsa.core.data;
 
-import java.util.Map;
-
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
+import java.util.Map;
+
 public class PageableTranslator {
+
+    private PageableTranslator(){}
+
     public static Pageable translate (Pageable pageable, Map<String, String> map){
         var orders = pageable.getSort().stream()
             .filter(order -> map.containsKey(order.getProperty())) // only valid orders
