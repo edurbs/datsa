0a1e0f583e3e0e76061d72573be3899c7c938369
Eduardo Soares <edurbs@gmail.com>
2025-10-31 15:25:25 -0300

    add: deprecated v0 api handler

--- src/main/java/com/github/edurbs/datsa/core/web/ApiDeadHandler.java
+++ src/main/java/com/github/edurbs/datsa/core/web/ApiDeadHandler.java
@@ -1,21 +1,21 @@
 package com.github.edurbs.datsa.core.web;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerInterceptor;
-
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
 public class ApiDeadHandler implements HandlerInterceptor {
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-//        if(request.getRequestURI().startsWith("/v1/")){
-//            response.setStatus(HttpStatus.GONE.value());
-//            return false;
-//        }
+    public boolean preHandle(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull Object handler) throws Exception {
+        if(request.getRequestURI().startsWith("/v0/")){
+            response.setStatus(HttpStatus.GONE.value());
+            return false;
+        }
         return true;
     }
 }
