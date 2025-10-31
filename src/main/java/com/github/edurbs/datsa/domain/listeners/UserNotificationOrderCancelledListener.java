6c69b85c71a73ff003810bb6dc619ed52ccc2776
Eduardo Soares <edurbs@gmail.com>
2025-10-31 16:05:45 -0300

    add: constructor injection

--- src/main/java/com/github/edurbs/datsa/domain/listeners/UserNotificationOrderCancelledListener.java
+++ src/main/java/com/github/edurbs/datsa/domain/listeners/UserNotificationOrderCancelledListener.java
@@ -1,18 +1,19 @@
 package com.github.edurbs.datsa.domain.listeners;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.event.TransactionalEventListener;
-
 import com.github.edurbs.datsa.domain.event.OrderCancelledEvent;
 import com.github.edurbs.datsa.domain.model.Order;
 import com.github.edurbs.datsa.domain.service.EmailSenderService;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 public class UserNotificationOrderCancelledListener {
 
-    @Autowired
-    EmailSenderService emailSenderService;
+    private final EmailSenderService emailSenderService;
+
+    public UserNotificationOrderCancelledListener(EmailSenderService emailSenderService) {
+        this.emailSenderService = emailSenderService;
+    }
 
     @TransactionalEventListener
     public void whenOrderCancelled(OrderCancelledEvent event){
