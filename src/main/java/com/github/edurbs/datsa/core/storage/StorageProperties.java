9b2d1b9463083db48029170e4bc6015c857081d0
Eduardo Soares <edurbs@gmail.com>
2025-10-31 16:13:05 -0300

    add: commented lines

--- src/main/java/com/github/edurbs/datsa/core/storage/StorageProperties.java
+++ src/main/java/com/github/edurbs/datsa/core/storage/StorageProperties.java
@@ -17,7 +17,6 @@ public class StorageProperties {
 
     private Local local = new Local();
     private S3 s3 = new S3();
-    //private StorageType storageType;
 
     public enum StorageType {
         LOCAL, S3
