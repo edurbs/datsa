93efbb64ce8550ba00e71eea83cf9b4b65678390
Eduardo Soares <edurbs@gmail.com>
2025-10-31 15:56:56 -0300

    add: constructor injection

--- src/main/java/com/github/edurbs/datsa/api/v1/mapper/PaymentMethodMapper.java
+++ src/main/java/com/github/edurbs/datsa/api/v1/mapper/PaymentMethodMapper.java
@@ -1,33 +1,29 @@
 package com.github.edurbs.datsa.api.v1.mapper;
 
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
-import org.springframework.lang.NonNull;
-import org.springframework.stereotype.Component;
-
 import com.github.edurbs.datsa.api.v1.LinksAdder;
 import com.github.edurbs.datsa.api.v1.controller.PaymentMethodController;
 import com.github.edurbs.datsa.api.v1.dto.input.PaymentMethodInput;
 import com.github.edurbs.datsa.api.v1.dto.output.PaymentMethodOutput;
 import com.github.edurbs.datsa.core.security.MySecurity;
 import com.github.edurbs.datsa.domain.model.PaymentMethod;
+import org.modelmapper.ModelMapper;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
 
 @Component
 public class PaymentMethodMapper extends RepresentationModelAssemblerSupport<PaymentMethod, PaymentMethodOutput> {
 
-    @Autowired
-    private ModelMapper modelMapper;
+    private final ModelMapper modelMapper;
+    private final LinksAdder linksAdder;
+    private final MySecurity mySecurity;
 
-    @Autowired
-    private LinksAdder linksAdder;
-
-    @Autowired
-    private MySecurity mySecurity;
-
-    public PaymentMethodMapper(){
+    public PaymentMethodMapper(ModelMapper modelMapper, LinksAdder linksAdder, MySecurity mySecurity) {
         super(PaymentMethodController.class, PaymentMethodOutput.class);
+        this.modelMapper = modelMapper;
+        this.linksAdder = linksAdder;
+        this.mySecurity = mySecurity;
     }
 
     public PaymentMethod toDomain(PaymentMethodInput input){
